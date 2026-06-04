9d320254566a133220527b647419f3b6208b0131
anleylafleur <anleylafleur@gmail.com>
2026-06-04 22:57:03 +1000

    Improve Azure SQL connection timeout settings

--- db.py
+++ db.py
@@ -4,14 +4,19 @@ import pyodbc
 pyodbc.pooling = False
 
 def get_connection():
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_NAME")
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+
     return pyodbc.connect(
         "DRIVER={ODBC Driver 18 for SQL Server};"
-        f"SERVER=tcp:{os.getenv('DB_SERVER')},1433;"
-        f"DATABASE={os.getenv('DB_NAME')};"
-        f"UID={os.getenv('DB_USER')};"
-        f"PWD={os.getenv('DB_PASSWORD')};"
+        f"SERVER=tcp:{server},1433;"
+        f"DATABASE={database};"
+        f"UID={user};"
+        f"PWD={password};"
         "Encrypt=yes;"
         "TrustServerCertificate=yes;"
         "Connection Timeout=120;"
-
+        "LoginTimeout=120;"
     )
